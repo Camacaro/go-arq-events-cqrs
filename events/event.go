00b9978caf39d77a3c3ac6c12558152f3de0b674
Jesus Camacaro <jesus.alejandr@gmail.com>
2022-05-04 15:56:16 -0400

    interfaces event store

---
+++ events/event.go
@@ -0,0 +1,38 @@
+package events
+
+import (
+	"context"
+
+	"github.com/Camacaro/cqrs/models"
+)
+
+/*
+	Repository pattern
+
+	Para eventos, se puede usar NATS, RabbitMQ, Kafka, etc.
+*/
+
+type EventStore interface {
+	Close()
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error            // Publicar cuando se ha creado un nuevo feed
+	SubscribeCreatedFeed(ctx context.Context) (<-chan CreateFeedMessage, error) // subscriba para cuando un nuevo feed a sido creado y se pueda leer
+	OnCreatedFeed(handler func(CreateFeedMessage)) error                        // Un callback que reaccione cuando un nuevo feed a sido creado
+}
+
+var eventStore EventStore
+
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+
+func SubscribeCreatedFeed(ctx context.Context) (<-chan CreateFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+
+func OnCreatedFeed(handler func(CreateFeedMessage)) error {
+	return eventStore.OnCreatedFeed(handler)
+}
---
+++ events/messages.go
@@ -0,0 +1,22 @@
+package events
+
+import "time"
+
+type Message interface {
+	Type() string
+}
+
+/*
+	Estos mensajes son los cuales seran
+	enviados entre los diferentes servicios
+*/
+type CreateFeedMessage struct {
+	ID          string    `json:"id"`
+	Tittle      string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func (m *CreateFeedMessage) Type() string {
+	return "create_feed"
+}
